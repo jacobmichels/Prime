9316168c001e5821738fe0ceaedfd8dde59dee94
Jacob Michels <jacob.michels2025@gmail.com>
2021-07-30 00:10:10 -0400

    add question models for endpoint input

---
+++ server/Models/Request/QuestionInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Models.Request
+{
+    public class QuestionInput
+    {
+        [Required]
+        public string QuestionText { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public bool ShowAnswerTextField { get; set; }
+        [Required]
+        public string GivenAnswer { get; set; }
+    }
+}
---
+++ server/Models/Request/QuestionSetInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Models.Request
+{
+    public class QuestionSetInput
+    {
+        public List<QuestionInput> Questions {  get; set; }
+        public string Title {  get; set; }
+        public bool Private { get; set; }
+
+    }
+}
