16fcc4523c66db406a1175871cbff1006a374b7a
Jacob Michels <jacob.michels2025@gmail.com>
2021-07-30 00:08:58 -0400

    add migrations for adding an owner field to QuestionsSet

---
+++ server/Migrations/20210730040739_QuestionSetOwner.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace server.Migrations
+{
+    public partial class QuestionSetOwner : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OwnerId",
+                table: "QuestionSets",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionSets_OwnerId",
+                table: "QuestionSets",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QuestionSets_Users_OwnerId",
+                table: "QuestionSets",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_QuestionSets_Users_OwnerId",
+                table: "QuestionSets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_QuestionSets_OwnerId",
+                table: "QuestionSets");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "QuestionSets");
+        }
+    }
+}
