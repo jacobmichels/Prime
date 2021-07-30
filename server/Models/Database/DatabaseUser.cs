ec2cacc97c2f739df787c4a2c9cd2e0b8b54f8e5
Jacob Michels <jacob.michels2025@gmail.com>
2021-07-29 21:20:57 -0400

    Update data annotations

--- server/Models/Database/DatabaseUser.cs
+++ server/Models/Database/DatabaseUser.cs
@@ -2,19 +2,18 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
-namespace server.Models
+namespace server.Models.Database
 {
     public class DatabaseUser
     {
         [Key]
         public int Id { get; set; }
-        
-        [MinLength(3)]
+        [Required]
         public string Username { get; set; }
-        
+        [Required]
         [EmailAddress]
         public string Email { get; set; }
-        
+        [Required]
         public string HashedPassword { get; set; }
     }
 }
