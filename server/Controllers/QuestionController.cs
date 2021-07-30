f4e29c2c7f4027cc2431062350dda7dc008057d9
Jacob Michels <jacob.michels2025@gmail.com>
2021-07-29 21:19:31 -0400

    add skeleton question controller

---
+++ server/Controllers/QuestionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using server.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        private readonly PrimeContext Db;
+
+        public QuestionController(PrimeContext db)
+        {
+            Db = db;
+        }
+
+    }
+}
