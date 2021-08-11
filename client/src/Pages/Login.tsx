bbf7f402145ab0fb3d60370d4769187303513219
Jacob Michels <jacob.michels2025@gmail.com>
2021-08-10 21:06:25 -0400

    change form background colour

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -35,104 +35,109 @@ export default function Login() {
       <Box
         p={3}
         borderRadius="10px"
-        bg="gray.700"
+        bg="blue.500"
         mt={{ base: "5", md: "175" }}
         ml="auto"
         mr="auto"
         maxW={{ base: "300px", md: "800px" }}
       >
-        <Text color="white" mb={4} fontWeight="bold" fontSize="3xl">
+        <Text color="white" ml={4} mb={4} fontWeight="bold" fontSize="3xl">
           Login
         </Text>
-        <Formik
-          initialValues={{}}
-          onSubmit={(values, actions) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              actions.setSubmitting(false);
-            }, 1000);
-          }}
-        >
-          {(props) => (
-            <Form>
-              <Field name="usernameOrEmail" validate={validateUsernameOrEmail}>
-                {({ field, form }: any) => (
-                  <FormControl
-                    isInvalid={
-                      form.errors.usernameOrEmail &&
-                      form.touched.usernameOrEmail
-                    }
-                  >
-                    <FormLabel color="white" htmlFor="usernameOrEmail">
-                      Username or Email
-                    </FormLabel>
-                    <Input
-                      autoComplete="off"
-                      {...field}
-                      id="usernameOrEmail"
-                      _placeholder={{ color: "white" }}
-                      placeholder="Username or Email"
-                    />
-                    <FormErrorMessage
-                      fontSize="md"
-                      // fontWeight="semibold"
-                      color="red"
+        <Box ml={3} mr={3}>
+          <Formik
+            initialValues={{}}
+            onSubmit={(values, actions) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                actions.setSubmitting(false);
+              }, 1000);
+            }}
+          >
+            {(props) => (
+              <Form>
+                <Field
+                  name="usernameOrEmail"
+                  validate={validateUsernameOrEmail}
+                >
+                  {({ field, form }: any) => (
+                    <FormControl
+                      isInvalid={
+                        form.errors.usernameOrEmail &&
+                        form.touched.usernameOrEmail
+                      }
                     >
-                      {form.errors.usernameOrEmail}
-                    </FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
-              <Field name="password" validate={validatePassword}>
-                {({ field, form }: any) => (
-                  <FormControl
-                    isInvalid={form.errors.password && form.touched.password}
-                  >
-                    <FormLabel color="white" htmlFor="password">
-                      Password
-                    </FormLabel>
-                    <Input
-                      autoComplete="off"
-                      {...field}
-                      id="password"
-                      _placeholder={{ color: "white" }}
-                      placeholder="Password"
-                    />
-                    <FormErrorMessage
-                      fontSize="md"
-                      // fontWeight="semibold"
-                      color="red"
+                      <FormLabel color="white" htmlFor="usernameOrEmail">
+                        Username or Email
+                      </FormLabel>
+                      <Input
+                        autoComplete="off"
+                        {...field}
+                        id="usernameOrEmail"
+                        _placeholder={{ color: "white" }}
+                        placeholder="Username or Email"
+                      />
+                      <FormErrorMessage
+                        fontSize="md"
+                        // fontWeight="bold"
+                        color="white"
+                      >
+                        ⚠️ {form.errors.usernameOrEmail}
+                      </FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field name="password" validate={validatePassword}>
+                  {({ field, form }: any) => (
+                    <FormControl
+                      isInvalid={form.errors.password && form.touched.password}
                     >
-                      {form.errors.password}
-                    </FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
-              <Button
-                m={3}
-                variant="solid"
-                color="white"
-                bg="gray.600"
-                _hover={{ bg: "gray.900", borderColor: "gray.900" }}
-                isLoading={props.isSubmitting}
-                type="submit"
-              >
-                Submit
-              </Button>
-              <Link to="/">
+                      <FormLabel color="white" htmlFor="password">
+                        Password
+                      </FormLabel>
+                      <Input
+                        autoComplete="off"
+                        {...field}
+                        id="password"
+                        _placeholder={{ color: "white" }}
+                        placeholder="Password"
+                      />
+                      <FormErrorMessage
+                        fontSize="md"
+                        // fontWeight="semibold"
+                        color="white"
+                      >
+                        ⚠️ {form.errors.password}
+                      </FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
                 <Button
                   m={3}
-                  color="white"
                   variant="solid"
-                  bg="gray.600"
-                  _hover={{ bg: "gray.900", borderColor: "gray.900" }}
+                  color="white"
+                  bg="blue.600"
+                  _hover={{ bg: "blue.900", borderColor: "blue.900" }}
+                  isLoading={props.isSubmitting}
+                  type="submit"
                 >
-                  Cancel
+                  Submit
                 </Button>
-              </Link>
-            </Form>
-          )}
-        </Formik>
+                <Link to="/">
+                  <Button
+                    m={3}
+                    color="white"
+                    variant="solid"
+                    bg="blue.600"
+                    _hover={{ bg: "blue.900", borderColor: "blue.900" }}
+                  >
+                    Cancel
+                  </Button>
+                </Link>
+              </Form>
+            )}
+          </Formik>
+        </Box>
       </Box>
     </>
   );
