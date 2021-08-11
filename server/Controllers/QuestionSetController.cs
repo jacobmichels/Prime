977dacc19a51b317a08fa820d72d9176ec055eae
Jacob Michels <jacob.michels2025@gmail.com>
2021-08-10 21:11:15 -0400

    add endpoints

--- server/Controllers/QuestionSetController.cs
+++ server/Controllers/QuestionSetController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using server.DataAccess;
+using server.Migrations;
 using server.Models.Database;
 using server.Models.Request;
 using System;
@@ -21,10 +24,15 @@ namespace server.Controllers
             Db = db;
         }
 
+        [Authorize]
         [HttpPost]
         public async Task<IActionResult> Create(QuestionSetInput input)
         {
-            DatabaseQuestionSet questionSet = new DatabaseQuestionSet(input.Title, input.Private);
+            if (string.IsNullOrWhiteSpace(HttpContext.User.Identity?.Name))
+            {
+                return Unauthorized();
+            }
+            DatabaseQuestionSet questionSet = new DatabaseQuestionSet(input.Title, input.Private, HttpContext.User.Identity.Name);
 
             foreach(var question in input.Questions)
             {
@@ -39,17 +47,90 @@ namespace server.Controllers
             return Ok();
         }
 
+        //Return all public questionsets.
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> GetAllPublic()
         {
-            return Ok(Db.QuestionSets.ToList());
+            var publicQuestionSets = await Db.QuestionSets.Where(questionSet => questionSet.Private == false).ToListAsync();
+
+            return Ok(publicQuestionSets);
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get(int id)
+        public async Task<IActionResult> GetById(int id)
         {
-            return Ok(Db.QuestionSets.First(questionset=>questionset.Id==id));
+            //if questionset is private, make sure the current user owns it.
+            var questionset = await Db.QuestionSets.FirstAsync(questionset => questionset.Id == id);
+            if (questionset.Private && questionset.OwnerName!=HttpContext.User.Identity?.Name)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(questionset);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetByTitle(string title)
+        {
+            //if questionset is private, make sure the current user owns it.
+            var questionset = await Db.QuestionSets.FirstAsync(questionset => questionset.Title == title);
+            if (questionset.Private && questionset.OwnerName != HttpContext.User.Identity?.Name)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(questionset);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetMine()
+        {
+            //if questionset is private, make sure the current user owns it.
+            var owner = HttpContext.User.Identity?.Name;
+            if (string.IsNullOrWhiteSpace(owner))
+            {
+                return Unauthorized();
+            }
+
+            var questionsets = await Db.QuestionSets.Where(questionset => questionset.OwnerName == owner).ToListAsync();
+            return Ok(questionsets);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            //make sure the current user is the owner of the question sets to be deleted
+            var owner = HttpContext.User.Identity?.Name;
+            if (string.IsNullOrWhiteSpace(owner))
+            {
+                return Unauthorized();
+            }
+
+            var questionSetToDelete = await Db.QuestionSets.Where(questionset=>questionset.OwnerName == owner && questionset.Id==id).Include(e=>e.Questions).FirstAsync();
+            if (questionSetToDelete == null)
+            {
+                return Unauthorized();
+            }
+            Db.QuestionSets.Remove(questionSetToDelete);
+            await Db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> Update(int id, QuestionSetInput input)
+        {
+            var questionset = await Db.QuestionSets.Where(questionset => questionset.Id == id).Include(e=>e.Questions).FirstAsync();
+            questionset.Private = input.Private;
+            questionset.Title = input.Title;
+            questionset.Questions.Clear();
+            //foreach (var question in input.Questions)
+            //{
+            //    var questionToAdd = new DatabaseQuestion(question.Title, question.QuestionText, question.GivenAnswer, question.ShowAnswerTextField);
+            //    questionset.Questions.Add(questionToAdd);
+            //}
+            await Db.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
