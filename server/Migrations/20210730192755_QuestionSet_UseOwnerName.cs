f2b8d113e0f957a0c276d8e409f3d984f353d14c
Jacob Michels <jacob.michels2025@gmail.com>
2021-08-10 21:08:36 -0400

    add migrations

---
+++ server/Migrations/20210730192755_QuestionSet_UseOwnerName.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace server.Migrations
+{
+    public partial class QuestionSet_UseOwnerName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_QuestionSets_Users_OwnerId",
+                table: "QuestionSets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_QuestionSets_OwnerId",
+                table: "QuestionSets");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "QuestionSets");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerName",
+                table: "QuestionSets",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerName",
+                table: "QuestionSets");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OwnerId",
+                table: "QuestionSets",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionSets_OwnerId",
+                table: "QuestionSets",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QuestionSets_Users_OwnerId",
+                table: "QuestionSets",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ server/Migrations/20210811001407_QuestionSet_AddForeignKeyForCascadeDelete.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace server.Migrations
+{
+    public partial class QuestionSet_AddForeignKeyForCascadeDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_QuestionSets_DatabaseQuestionSetId",
+                table: "Questions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DatabaseQuestionSetId",
+                table: "Questions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_QuestionSets_DatabaseQuestionSetId",
+                table: "Questions",
+                column: "DatabaseQuestionSetId",
+                principalTable: "QuestionSets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_QuestionSets_DatabaseQuestionSetId",
+                table: "Questions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DatabaseQuestionSetId",
+                table: "Questions",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_QuestionSets_DatabaseQuestionSetId",
+                table: "Questions",
+                column: "DatabaseQuestionSetId",
+                principalTable: "QuestionSets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
