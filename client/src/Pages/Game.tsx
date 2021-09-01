06f08c8478a2c311e958ee4dae5f199e9b903aba
Jacob Michels <jacob.michels2025@gmail.com>
2021-09-01 02:37:32 -0400

    add basic guid validation

--- client/src/Pages/Game.tsx
+++ client/src/Pages/Game.tsx
@@ -1,5 +1,40 @@
-import React from "react";
+import { Center, Spinner, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-export default function Games() {
-  return <div>Games</div>;
+export default function Game() {
+  let { id }: any = useParams();
+  let [fetching, setFetchingState] = useState<boolean>(true);
+
+  useEffect(() => {
+    const guidRegex =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    if (!guidRegex.test(id)) {
+      alert("bad guid");
+      setFetchingState(false);
+    }
+  }, [id]);
+
+  return (
+    <>
+      <Center h="100%">
+        {fetching ? (
+          <>
+            <Text>Fetching game info...</Text>
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="blue.500"
+              size="xl"
+              m={10}
+            />
+          </>
+        ) : (
+          <Text>Done fetching</Text>
+        )}
+      </Center>
+    </>
+  );
 }
