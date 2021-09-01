4c7652757494de0a666f793dff0298c021e371e3
Jacob Michels <jacob.michels2025@gmail.com>
2021-08-31 23:26:31 -0400

    create http controller and signalr hub for games

---
+++ server/Controllers/GameController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using server.DataAccess;
+using server.Migrations;
+using server.Models.Database;
+using server.Models.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class GameController : ControllerBase
+    {
+        private readonly PrimeContext Db;
+        public GameController(PrimeContext db)
+        {
+            Db = db;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> CreateGame(string gameName, bool isPrivate)
+        {
+            if (await GameWithNameExists(gameName))
+            {
+                return Problem(title:"Game name in use.", detail: $"A game with the name \"{gameName}\" already exists. Please choose another name.", statusCode:400);
+            }
+            var user = await Db.Users.FirstOrDefaultAsync(user => user.Username == HttpContext.User.Identity.Name);
+            if(user is null)
+            {
+                return Unauthorized();
+            }
+            var newGame = new DatabaseGame(gameName, isPrivate, user);
+            Db.Games.Add(newGame);
+
+            return Ok();
+        }
+
+        private async Task<bool> GameWithNameExists(string gameName)
+        {
+            var game = await Db.Games.FirstOrDefaultAsync(e => e.Name == gameName);
+            return game != null;
+        }
+        
+    }
+}
---
+++ server/Controllers/GameHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace server.Controllers
+{
+    public class GameHub: Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnConnectedAsync();
+            Console.WriteLine($"User connected. UserIdentifier: {Context.UserIdentifier} ConnectionId: {Context.ConnectionId} User: {Context.User.Identity.Name}");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnDisconnectedAsync(exception);
+            Console.WriteLine($"User disconnected. UserIdentifier: {Context.UserIdentifier} ConnectionId: {Context.ConnectionId} User: {Context.User.Identity.Name}");
+        }
+    }
+}
+
