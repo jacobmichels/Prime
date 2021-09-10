eb95900b8c8dc84e7d7abe69174aa3987d6b469a
Jacob Michels <jacob.michels2025@gmail.com>
2021-09-09 22:43:38 -0400

    make game on form submit

--- client/src/Components/HostModal.tsx
+++ client/src/Components/HostModal.tsx
@@ -14,37 +14,84 @@ import {
   ModalHeader,
   ModalOverlay,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
+import { BASE_URL } from "../Util/BaseURL";
 
 export default function HostModal(props: any) {
+  const [gameName, setGameName] = useState("");
+  const [gamePrivate, setGamePrivate] = useState(false);
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleCheckbox = () => {
+    setGamePrivate(!gamePrivate);
+  };
+
   return (
     <Modal isOpen={props.isOpen} onClose={props.onClose}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Host a game</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>
-          <FormControl>
-            <FormLabel>Session Name</FormLabel>
-            <Input placeholder="Name" />
-          </FormControl>
+        <form
+          onSubmit={async (event) => {
+            event.preventDefault();
+            setIsSubmitting(true);
+            let res = await fetch(
+              BASE_URL +
+                `/Game/CreateGame?gameName=${gameName}&isPrivate=${gamePrivate}`,
+              {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+            if (res.status === 200) {
+              alert("game created");
+            } else {
+              alert("game creation failed");
+              console.log(await res.text());
+            }
 
-          <FormControl mt={2}>
-            <Center>
-              <HStack>
-                <FormLabel>Private</FormLabel>
-                <Checkbox />
-              </HStack>
-            </Center>
-          </FormControl>
-        </ModalBody>
+            setIsSubmitting(false);
+          }}
+        >
+          <ModalBody>
+            <FormControl>
+              <FormLabel>Session Name</FormLabel>
+              <Input
+                value={gameName}
+                onChange={(event) => {
+                  setGameName(event.target.value);
+                }}
+                placeholder="Name"
+              />
+            </FormControl>
 
-        <ModalFooter>
-          <Button colorScheme="blue" mr={3}>
-            Start
-          </Button>
-          <Button onClick={props.onClose}>Cancel</Button>
-        </ModalFooter>
+            <FormControl mt={2}>
+              <Center>
+                <HStack>
+                  <FormLabel>Private</FormLabel>
+                  <Checkbox isChecked={gamePrivate} onChange={handleCheckbox} />
+                </HStack>
+              </Center>
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              isLoading={isSubmitting}
+              type="submit"
+              colorScheme="blue"
+              mr={3}
+            >
+              Start
+            </Button>
+            <Button onClick={props.onClose}>Cancel</Button>
+          </ModalFooter>
+        </form>
       </ModalContent>
     </Modal>
   );
